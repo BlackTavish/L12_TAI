9ca987101cd25c7e667aea77f5fa803a1cf2c697
BlackTavish <kubalos98@gmail.com>
2021-06-05 08:49:48 +0200

    Final commit

--- blog-server/app/config.js
+++ blog-server/app/config.js
@@ -1,6 +1,6 @@
 const config = {
   port: process.env.PORT || 3000,
-  databaseUrl: process.env.MONGODB_URI || 'mongodb://database/mean-docker',
+  databaseUrl: process.env.MONGODB_URI || 'mongodb://localhost:27017/blog',
   JwtSecret: process.env.JWT_SECRET || 'secret'
 };
 
